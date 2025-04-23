bb8815668e1f340d743e259b26998387397a7df6
hobaaaa <denizgokbudak@gmail.com>
2025-04-23 16:59:13 +0300

    implement interview generation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import { isAuthenticated } from "@/lib/actions/auth.action";
 import { redirect } from "next/navigation";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
-  const isUserAuthenticated = isAuthenticated();
+  const isUserAuthenticated = await isAuthenticated();
   if (!isUserAuthenticated) redirect("/sign-in");
   return (
     <div className="root-layout">
